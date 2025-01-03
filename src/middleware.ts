a5e53f66ebec3fc9d61940f9a3b5cb2c60479117
mihir22c <mihirbavishi111@gmail.com>
2025-01-03 13:07:06 +0100

    Add Home page and update layout metadata; modify middleware for token handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,8 +6,12 @@ export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
 
     const isPublicPath = path === "/login" || path === "/signup" || path === "/verifyemail"
+    console.log("9: path", path, isPublicPath);
+
 
     const token = request.cookies.get('token')?.value || ""
+    console.log("13: token", token);
+
 
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/', request.url))
@@ -16,7 +20,7 @@ export function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/login', request.url))
 
     }
-    return NextResponse.redirect(new URL('/home', request.url))
+    // return NextResponse.redirect(new URL('/home', request.url))
 }
 
 // See "Matching Paths" below to learn more
